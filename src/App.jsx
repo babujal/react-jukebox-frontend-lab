0b2583f82369a3f79dd67bcaf76da537f6e58b03
babujal <dercamar1@gmail.com>
2024-10-20 19:31:59 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './App.css'
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/tracks`;
+
+function App() {
+  return (<h1>Hello world!</h1>);
+}
+
+export default App
