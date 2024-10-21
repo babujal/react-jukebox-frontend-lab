03f56f1c82c850961359a3f353a589be25c2ad1a
babujal <dercamar1@gmail.com>
2024-10-20 20:47:56 -0400

    Figured out components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Card from './components/Card';
+import TrackList from './components/TrackList';
 import './App.css'
 
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/tracks`;
 
+const cardNumber = 4
+
 function App() {
-  return (<h1>Hello world!</h1>);
+  return (
+    <>
+      <h1>Track List:</h1>
+      <Routes>
+        <Route path='/' element={<h1>Hello World</h1>} />
+        <Route path='/tracks' element={< Card card={cardNumber} />} />
+        {/* <Route path='/traks/new' element={<NewTrack />} />
+        <Route path='/traks/:id' element={<EditTrack />} /> */}
+        <Route path='*' element={<h2>Whoops, nothing here!</h2>} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+const Card = (props) => {
+    return (
+        <div>
+            <h1>I'm {`${props.card}`} cards</h1>
+        </div>
+    )
+}
+
+export default Card
